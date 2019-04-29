d891a430d51480f34653410763dd8b8a585cdd73
Matthew DEVILLE <mdeville@student.42.fr>
2019-04-29 13:44:33 +0200

    Add lmezard fun script

---
+++ scripts/lmezard_ftp.py
@@ -0,0 +1,22 @@
+#!/usr/bin/env python3
+
+import re
+import os
+import sys
+
+FILE_NO = r"file(\d+)"
+FILE_NO_REG = re.compile(FILE_NO)
+
+ordered = dict()
+
+for root, dirs, files in os.walk(sys.argv[1]):
+    for pcap_file in files:
+        f = open(os.path.join(root, pcap_file), "r")
+        tmp = f.read()
+        matches = FILE_NO_REG.search(tmp)
+        no = matches.group(1)
+        ordered[int(no)] = tmp
+        f.close()
+
+for i in range(1, len(files) + 1):
+    print(ordered[i])
